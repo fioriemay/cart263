645018cfd3fbef079fb5fcbb5eca6a2ec1f9ead3
fioriemay <f_oussel@live.concordia.ca>
2024-01-22 17:59:41 -0500

    A1:SausageDog: Set up folder, images and title

---
+++ Activities/A1_SausageDog/js/script.js
@@ -0,0 +1,26 @@
+/**
+Where is Sausage Dog
+Fiorie Rousselot-Barbe
+
+Followed the week 1 Activity
+*/
+
+"use strict";
+
+
+
+function preload() {
+
+}
+
+
+
+function setup() {
+
+}
+
+
+
+function draw() {
+
+}
