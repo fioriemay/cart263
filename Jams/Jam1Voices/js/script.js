a9e0e8d2f4e72698ef630dc41d0d46b11fb34e49
fioriemay <123784333+fioriemay@users.noreply.github.com>
2024-01-25 15:03:52 -0500

    Jam1: Finished set up

--- Jams/Jam1Voices/js/script.js
+++ Jams/Jam1Voices/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+Jam 1
+Fiorie Rousselot-Barbe / 40177306
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -9,25 +9,19 @@ author, and this description to match your project!
 "use strict";
 
 
-/**
-Description of preload
-*/
+
 function preload() {
 
 }
 
 
-/**
-Description of setup
-*/
+
 function setup() {
 
 }
 
 
-/**
-Description of draw()
-*/
+
 function draw() {
 
 }
