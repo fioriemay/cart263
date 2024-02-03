085ed13366f372b0b23e50fe7dd9a2c63007ac95
fioriemay <f_oussel@live.concordia.ca>
2024-02-03 14:19:07 -0500

    Reorganizing Files

---
+++ Experiments/Experiment/js/script.js
@@ -0,0 +1,32 @@
+/**
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+const speechRecognizer = new p5.SpeechRec();
+
+function preload() {
+
+}
+
+
+
+function setup() {
+
+}
+
+
+
+function draw() {
+
+}
+
+function mousePressed(){
+
+    
+}
