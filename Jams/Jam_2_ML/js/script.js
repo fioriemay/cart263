25f0d21a9a4d2d318a8a79b1c4859754932eb367
fioriemay <f_oussel@live.concordia.ca>
2024-02-20 23:04:42 -0500

    Jam2: initiating sentiment & speech and debug functions

--- Jams/Jam_2_ML/js/script.js
+++ Jams/Jam_2_ML/js/script.js
@@ -7,7 +7,17 @@ Tell the computer a sentence about your day, it will judge whether it is a good 
 
 "use strict";
 
+// our variable to initialize sentiment
+let sentiment;
 
+// to listen to user input
+let userVoice = new p5.SpeechRec();
+
+//to tell the user how their day went
+const computerVoice = new p5.Speech();
+
+//state can be title or simulation
+let state = 'simulation';
 
 function preload() {
 
@@ -17,10 +27,40 @@ function preload() {
 
 function setup() {
 
+    //initializing sentiment
+    sentiment = ml5.sentiment('movieReviews', modelReady);
+
+    //initializing voice input
+    userVoice.onResult = logResult;
+    userVoice.continuous = true;
+    userVoice.start();
+
+    //canvas setup
+    createCanvas(500, 500);
 }
 
 
 
 function draw() {
+    
+}
+
+
+
+////------debug functions------////
+
+function logResult(){
+
+    if (userVoice.resultValue === true) {
+        
+        console.log(userVoice.resultString);
 
 }
+
+}
+
+
+function modelReady() {
+    //callback function to confirm that sentiment was properly initialized
+    console.log('Model Ready!');
+  }
