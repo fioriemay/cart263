06dd0bf7dd0f744fbededb35d83693c82df4893a
fioriemay <f_oussel@live.concordia.ca>
2024-02-20 21:56:43 -0500

    Jam2: Setting up files

---
+++ Jams/Jam_2_ML/js/script.js
@@ -0,0 +1,26 @@
+/**
+Talk to me
+Fiorie Rousselot-Barbe
+
+Tell the computer a sentence about your day, it will judge whether it is a good or bad day.
+*/
+
+"use strict";
+
+
+
+function preload() {
+
+}
+
+
+
+function setup() {
+
+}
+
+
+
+function draw() {
+
+}
