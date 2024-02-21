dec96744ca39abd9baf76ed8dbaf76dbd3d651bc
fioriemay <f_oussel@live.concordia.ca>
2024-02-21 11:02:02 -0500

    Jam2: added states, setup sentiment variables

--- Jams/Jam_2_ML/js/script.js
+++ Jams/Jam_2_ML/js/script.js
@@ -3,6 +3,7 @@ Talk to me
 Fiorie Rousselot-Barbe
 
 Tell the computer a sentence about your day, it will judge whether it is a good or bad day.
+The computer judges in its own way, and might see things differently than you do. Have fun!
 */
 
 "use strict";
@@ -17,7 +18,7 @@ let userVoice = new p5.SpeechRec();
 const computerVoice = new p5.Speech();
 
 //state can be title or simulation
-let state = 'simulation';
+let state = 'title';
 
 function preload() {
 
@@ -31,7 +32,7 @@ function setup() {
     sentiment = ml5.sentiment('movieReviews', modelReady);
 
     //initializing voice input
-    userVoice.onResult = logResult;
+    userVoice.onResult = getSentimentVal;
     userVoice.continuous = true;
     userVoice.start();
 
@@ -42,22 +43,95 @@ function setup() {
 
 
 function draw() {
+
+    //states for title screen and simulation
+    if(state === 'title'){
+
+        title();
+
+    }
+
+    else if (state === 'simulation'){
+
+        simulation();
+
+    }
     
 }
 
+function title(){
+
+    push();
+    background(93, 62, 122);
+    textSize(48);
+    fill(10, 4, 15);
+    textAlign(CENTER, CENTER);
+    textStyle(BOLD);
+    text('Rate my day!', width/2, 200);
+    textSize(30);
+    text('Click to Begin!', width/2, 460);
+    textStyle(NORMAL);
+    textSize(25);
+    text('Tell the computer a sentence about your day,', width/2, 240);
+    text('It will judge whether it is a good or bad day.', width/2, 260);
+    pop();
+
+}
+
+function simulation(){
+
+    push();
+    background(93, 62, 122);
+    textSize(48);
+    fill(10, 4, 15);
+    textAlign(CENTER, CENTER);
+    textStyle(BOLD);
+    text('test', width/2, 200);
+   
+    getSentimentVal();
+
+}
+
+function getSentimentVal(){
+
+    //what the user says goes into this constant variable
+    const inputText = userVoice.resultString;
+    //if the user speaks, the console logs what they said
+    if (userVoice.resultValue === true) {
+        
+                console.log(inputText);
+        
+         }
+
+    //the numerical value created by sentiment, based off of what the user said.
+    const judgement = sentiment.predict(inputText);
+    //we also log the value sentiment gave to the user's input
+    console.log(judgement);
+}
+
+function mousePressed(){
+
+    //if user cliks on the title screen, the simulation will begin
+    if (state === 'title') {
+
+        state = 'simulation';
+
+    }
+}
 
 
 ////------debug functions------////
 
-function logResult(){
+// function logResult(){
 
-    if (userVoice.resultValue === true) {
+//     //used to make sure voice input worked properly
+//     if (userVoice.resultValue === true) {
         
-        console.log(userVoice.resultString);
+//         console.log(userVoice.resultString);
 
-}
+// }
 
-}
+// }
 
 
 function modelReady() {
